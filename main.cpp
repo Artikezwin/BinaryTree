0ebcb92a9c313fc7995dd5e488549e4bc8a33719
Artemka <Inkognito@example.com>
2022-11-12 18:40:56 +0300

    Небольшое исправление формата работы программы

--- main.cpp
+++ main.cpp
@@ -17,15 +17,33 @@ void append(Tree &tree){
     tree.insert(1.62);
 }
 
+void inp(Tree &tree){
+    int n=0;
+    float a;
+    for(int i=0;i<10;i++){
+        cin>>a;
+        tree.insert(a);
+    }
+}
+
 int main() {
     system("chcp 65001");
     Tree tree = Tree();
-    append(tree);
+    cout<<"Выберете, какой вы хотите ввод\n";
+    cout<<"1 - тестовый набор данных, 2 - ввод вручную\n";
+    int strelka;
+    cin>>strelka;
+    switch (strelka) {
+        case 1:
+            append(tree);
+            break;
+        case 2:
+            inp(tree);
+            break;
+    }
     tree.output(tree.getRoot());
     cout<<"\n";
     tree.average(tree.getRoot());
-    cout<<"\n";
-    cout<<tree.getRoot()->getKey();
     cout<<"\n";
     cout<<"Длина от корня до 15.5 - "<<tree.length(15.5);
     cout<<"\n";
